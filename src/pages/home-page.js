cb9457da738ea6a5b3b9856d9683dda61978d181
Kwasi Antwi Baayeh <saaddae003@st.ug.edu.gh>
2023-04-18 15:38:58 +0000

    Dispatch action to get global data

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import React, {
+  Suspense,
+  lazy,
+  useCallback,
+  useEffect,
+  useState,
+  useTransition,
+} from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllCountries, globalInfo } from '../redux/metrics/metricSlice';
+import styles from '../styles/home.module.css';
 
-const Home = () => (
-  <div id="home"> Home</div>
-);
+const Countries = lazy(() => import('../features/Countries/countries'));
+
+const Home = () => {
+  const [data, setData] = useState(null);
+  const [isPending, startTransition] = useTransition();
+  const dispatch = useDispatch();
+
+  const fetchData = useCallback(() => {
+    dispatch(globalInfo()).then((res) => {
+      if (res.meta.requestStatus === 'fulfilled') {
+        setData(res.payload);
+      }
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    startTransition(() => {
+      fetchData();
+    });
+  }, [dispatch, fetchData, fetchCountries]);
 
 export default Home;
