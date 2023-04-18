9c45c639cd1ba4ff633d438b0535c5fc16954400
Kwasi Antwi Baayeh <saaddae003@st.ug.edu.gh>
2023-04-18 09:04:05 +0000

    Add a splash screen

---
+++ src/pages/splash-page.js
@@ -0,0 +1,8 @@
+import styles from '../styles/Splash.module.css';
+
+const SplashScreen = () => (
+  <section className={styles['splash-container']}>
+    <h1 className={styles.logo}>Covidio</h1>
+  </section>
+);
+export default SplashScreen;
