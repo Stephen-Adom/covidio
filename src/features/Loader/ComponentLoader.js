38fd33b801f43a6915844539e1ebfc1073bc7b2b
Kwasi Antwi Baayeh <saaddae003@st.ug.edu.gh>
2023-04-18 21:28:42 +0000

    Create a loader component

---
+++ src/features/Loader/ComponentLoader.js
@@ -0,0 +1,7 @@
+const ComponentLoader = () => (
+  // const variable;
+  <section className="grid h-screen place-items-center">
+    <div className="spinner" />
+  </section>
+);
+export default ComponentLoader;
