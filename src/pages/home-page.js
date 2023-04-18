3cb59286295d0ee5c2c0eadb61e16ac4616de0f9
Kwasi Antwi Baayeh <saaddae003@st.ug.edu.gh>
2023-04-18 20:49:22 +0000

    Update the Dashboard

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,14 +1,14 @@
 import React, {
   Suspense,
   lazy,
-  useCallback,
   useEffect,
   useState,
   useTransition,
 } from 'react';
 import { useDispatch } from 'react-redux';
+import PageHeader from '../features/Header/PageHeader';
+import ComponentLoader from '../features/Loader/ComponentLoader';
 import { getAllCountries, globalInfo } from '../redux/metrics/metricSlice';
-import styles from '../styles/home.module.css';
 
 const Countries = lazy(() => import('../features/Countries/countries'));
 
@@ -18,28 +18,30 @@ const Home = () => {
   const [isPending, startTransition] = useTransition();
   const dispatch = useDispatch();
 
-  const fetchData = useCallback(() => {
-    dispatch(globalInfo()).then((res) => {
-      if (res.meta.requestStatus === 'fulfilled') {
-        setData(res.payload);
-      }
+  const fetchData = () => {
+    startTransition(() => {
+      dispatch(globalInfo()).then((res) => {
+        if (res.meta.requestStatus === 'fulfilled') {
+          setData(res.payload);
+        }
+      });
     });
-  }, [dispatch]);
+  };
 
-  const fetchCountries = useCallback(() => {
-    dispatch(getAllCountries()).then((res) => {
-      if (res.meta.requestStatus === 'fulfilled') {
-        setCountries(res.payload);
-      }
+  const fetchCountries = () => {
+    startTransition(() => {
+      dispatch(getAllCountries()).then((res) => {
+        if (res.meta.requestStatus === 'fulfilled') {
+          setCountries(res.payload);
+        }
+      });
     });
-  }, [dispatch]);
+  };
 
   useEffect(() => {
-    startTransition(() => {
-      fetchData();
-      fetchCountries();
-    });
-  }, [dispatch, fetchData, fetchCountries]);
+    fetchData();
+    fetchCountries();
+  }, [dispatch]);
 
   return (
     <section className="text-red-600">
@@ -47,49 +49,10 @@ const Home = () => {
         <p>Loading...</p>
       ) : (
         <>
-          <header className={styles.header}>
-            <h1 className="text-center text-2xl font-bold tracking-wide">
-              Global Information
-            </h1>
-
-            {data && (
-              <section className="mt-3 flex flex-wrap justify-center gap-4">
-                <div className={styles.card}>
-                  <p className="text-green-300 font-bold">
-                    {data.cases.toLocaleString()}
-                  </p>
-                  <p>Total Cases</p>
-                </div>
-                <div className={styles.card}>
-                  <p className="text-yellow-100 font-bold">
-                    {data.active.toLocaleString()}
-                  </p>
-                  <p>Active Cases</p>
-                </div>
-                <div className={styles.card}>
-                  <p className="text-red-500 font-bold">
-                    {data.deaths.toLocaleString()}
-                  </p>
-                  <p>Total Deaths</p>
-                </div>
-                <div className={styles.card}>
-                  <p className="text-green-300 font-bold">
-                    {data.todayCases.toLocaleString()}
-                  </p>
-                  <p>New Cases</p>
-                </div>
-                <div className={styles.card}>
-                  <p className="text-yellow-500 font-bold">
-                    {data.critical.toLocaleString()}
-                  </p>
-                  <p>Critical Cases</p>
-                </div>
-              </section>
-            )}
-          </header>
+          <PageHeader data={data} />
 
           <Suspense fallback={<ComponentLoader />}>
-            <Countries countries={countries} />
+            {countries && <Countries countries={countries} />}
           </Suspense>
         </>
       )}
