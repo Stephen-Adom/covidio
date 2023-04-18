fcd40714618bc55a3a845de8f2625f003bf78dba
Kwasi Antwi Baayeh <saaddae003@st.ug.edu.gh>
2023-04-18 13:38:16 +0000

    Create the initial state of the application

--- src/redux/metrics/metricSlice.js
+++ src/redux/metrics/metricSlice.js
@@ -1,3 +1,13 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchGlobalData } from '../api/endpoints';
+
+const initialState = {
+  global: {},
+  countries: [],
+  status: '',
+  error: '',
+};
+
 export const globalInfo = createAsyncThunk('metrics/all', async (thunkAPI) => {
   try {
     const response = await fetchGlobalData();
